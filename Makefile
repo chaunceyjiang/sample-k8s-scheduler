7be87eda2fe18068aa09953db93ab465552dbf43
chaunceyjiang <chaunceyjiang@gmail.com>
2022-02-15 11:25:25 +0800

    deploy in k8s
    Signed-off-by: chaunceyjiang <chaunceyjiang@gmail.com>

--- Makefile
+++ Makefile
@@ -1,15 +1,16 @@
 
 IMG_NAME ?=chaunceyjiang/sample-k8s-scheduler
-IMG_VERSION ?=v0.0.1
-
+IMG_VERSION ?=v0.0.12
+IMG ?=${IMG_NAME}:${IMG_VERSION}
 
 binary:
 	go build -o sample-k8s-scheduler main.go
 
 build: binary
-	docker build -t ${IMG_NAME}:${IMG_VERSION} .
+	sed -i 's@image: .*@image: '"${IMG}"'@' deploy/sample-scheduler.yaml
+	docker build -t ${IMG} .
 deploy: build
 	kubectl apply -f deploy/sample-scheduler.yaml
-
-release: build
-	docker push ${IMG_NAME}:${IMG_VERSION}
+push: build
+	docker push ${IMG}
+release: build push deploy
--- pkg/plugins/plugins.go
+++ pkg/plugins/plugins.go
@@ -9,10 +9,24 @@ import (
 )
 
 // Name 插件名称
-const Name = "sample-plugin"
+const Name = "sample-scheduler"
 
 type Sample struct {
 	handle framework.Handle
+}
+
+// PreFilterPlugin is an interface that must be implemented by "prefilter" plugins
+var _ framework.PreFilterPlugin = &Sample{}
+
+func (s *Sample) PreFilterExtensions() framework.PreFilterExtensions {
+	klog.V(3).Infof("PreFilterExtensions....")
+	// PreFilterExtensions returns a PreFilterExtensions interface if the plugin implements one,
+	// or nil if it does not. A Pre-filter plugin can provide extensions to incrementally
+	// modify its pre-processed info. The framework guarantees that the extensions
+	// AddPod/RemovePod will only be called after PreFilter, possibly on a cloned
+	// CycleState, and may call those functions more than once before calling
+	// Filter again on a specific node.
+	return nil
 }
 
 func (s *Sample) Name() string {
@@ -23,6 +37,7 @@ func (s *Sample) PreFilter(ctx context.Context, state *framework.CycleState, p *
 	klog.V(3).Infof("prefilter pod: %v", p.Name)
 	return framework.NewStatus(framework.Success, "")
 }
+
 func New(obj runtime.Object, handle framework.Handle) (framework.Plugin, error) {
 	return &Sample{handle: handle}, nil
 }
